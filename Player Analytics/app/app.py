f1681d71253315134a97e804b3f2764184fcc7f9
matchu46 <mattedelson46@gmail.com>
2026-04-04 02:52:20 -0700

    Remove duplicate ads.txt route

--- Player Analytics/app/app.py
+++ Player Analytics/app/app.py
@@ -1301,11 +1301,6 @@ def robots():
     return app.send_static_file("robots.txt")
 
 
-@app.route("/ads.txt")
-def ads_txt():
-    return app.send_static_file("ads.txt")
-
-
 @app.route("/privacy")
 def privacy():
     return render_template("privacy.html")
